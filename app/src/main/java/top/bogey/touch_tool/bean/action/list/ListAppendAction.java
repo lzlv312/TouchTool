07cb2e943dcbb3dc50c1c6f4f80e08b5be027359
haihe8177 <yyy51306@gmail.com>
2026-09-20 14:51:02 +0800

    实现多个列表/字典拼接

--- app/src/main/java/top/bogey/touch_tool/bean/action/list/ListAppendAction.java
+++ app/src/main/java/top/bogey/touch_tool/bean/action/list/ListAppendAction.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import top.bogey.touch_tool.R;
 import top.bogey.touch_tool.bean.action.ActionType;
 import top.bogey.touch_tool.bean.pin.Pin;
+import top.bogey.touch_tool.bean.pin.pin_objects.PinAdd;
 import top.bogey.touch_tool.bean.pin.pin_objects.pin_execute.PinExecute;
 import top.bogey.touch_tool.bean.pin.pin_objects.pin_list.PinList;
 import top.bogey.touch_tool.service.TaskRunnable;
@@ -17,25 +19,31 @@ import top.bogey.touch_tool.service.TaskRunnable;
 public class ListAppendAction extends ListExecuteAction {
     private final transient Pin listPin = new Pin(new PinList());
     private final transient Pin list2Pin = new Pin(new PinList());
+    private final transient Pin addPin = new Pin(new PinAdd(Arrays.asList(list2Pin)), R.string.pin_add_pin);
     private final transient Pin resultPin = new Pin(new PinList(), R.string.pin_boolean_result, true);
 
     public ListAppendAction() {
         super(ActionType.LIST_APPEND);
-        addPins(listPin, list2Pin, resultPin);
+        addPins(listPin, list2Pin, addPin, resultPin);
     }
 
     public ListAppendAction(JsonObject jsonObject) {
         super(jsonObject);
-        reAddPins(listPin, list2Pin, resultPin);
+        reAddPin(listPin);
+        reAddPins(list2Pin);
+        reAddPin(addPin);
+        reAddPin(resultPin);
     }
 
     @Override
     public void execute(TaskRunnable runnable, Pin pin) {
-        PinList list = getPinValue(runnable, listPin);
-        PinList list2 = getPinValue(runnable, list2Pin);
-        PinList pinList = resultPin.getValue(PinList.class);
-        pinList.addAll(list);
-        pinList.addAll(list2);
+        PinList result = resultPin.getValue(PinList.class);
+        for (Pin p : getListPins()) {
+            PinList list = getPinValue(runnable, p);
+            if (list != null) {
+                result.addAll(list);
+            }
+        }
         executeNext(runnable, outPin);
     }
 
@@ -46,10 +54,25 @@ public class ListAppendAction extends ListExecuteAction {
         }
     }
 
+    private List<Pin> getListPins() {
+        List<Pin> pins = new ArrayList<>();
+        for (Pin pin : getPins()) {
+            if (pin == addPin) {
+                break;
+            }
+            if (pin.isSameClass(PinList.class)) {
+                pins.add(pin);
+            }
+        }
+        return pins;
+    }
+
     @NonNull
     @Override
     public List<Pin> getDynamicTypePins() {
-        return Arrays.asList(listPin, list2Pin, resultPin);
+        List<Pin> pins = new ArrayList<>(getListPins());
+        pins.add(resultPin);
+        return pins;
     }
 
 }
--- app/src/main/java/top/bogey/touch_tool/bean/action/map/MapAppendAction.java
+++ app/src/main/java/top/bogey/touch_tool/bean/action/map/MapAppendAction.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import top.bogey.touch_tool.R;
 import top.bogey.touch_tool.bean.action.ActionType;
 import top.bogey.touch_tool.bean.pin.Pin;
+import top.bogey.touch_tool.bean.pin.pin_objects.PinAdd;
 import top.bogey.touch_tool.bean.pin.pin_objects.PinMap;
 import top.bogey.touch_tool.bean.pin.pin_objects.pin_execute.PinExecute;
 import top.bogey.touch_tool.service.TaskRunnable;
@@ -17,25 +19,31 @@ import top.bogey.touch_tool.service.TaskRunnable;
 public class MapAppendAction extends MapExecuteAction {
     private final transient Pin mapPin = new Pin(new PinMap());
     private final transient Pin map2Pin = new Pin(new PinMap());
+    private final transient Pin addPin = new Pin(new PinAdd(Arrays.asList(map2Pin)), R.string.pin_add_pin);
     private final transient Pin resultPin = new Pin(new PinMap(), R.string.pin_boolean_result, true);
 
     public MapAppendAction() {
         super(ActionType.MAP_APPEND);
-        addPins(mapPin, map2Pin, resultPin);
+        addPins(mapPin, map2Pin, addPin, resultPin);
     }
 
     public MapAppendAction(JsonObject jsonObject) {
         super(jsonObject);
-        reAddPins(mapPin, map2Pin, resultPin);
+        reAddPin(mapPin);
+        reAddPins(map2Pin);
+        reAddPin(addPin);
+        reAddPin(resultPin);
     }
 
     @Override
     public void execute(TaskRunnable runnable, Pin pin) {
-        PinMap map = getPinValue(runnable, mapPin);
-        PinMap map2 = getPinValue(runnable, map2Pin);
-        PinMap pinMap = resultPin.getValue(PinMap.class);
-        pinMap.putAll(map);
-        pinMap.putAll(map2);
+        PinMap result = resultPin.getValue(PinMap.class);
+        for (Pin p : getMapPins()) {
+            PinMap map = getPinValue(runnable, p);
+            if (map != null) {
+                result.putAll(map);
+            }
+        }
         executeNext(runnable, outPin);
     }
 
@@ -46,10 +54,25 @@ public class MapAppendAction extends MapExecuteAction {
         }
     }
 
+    private List<Pin> getMapPins() {
+        List<Pin> pins = new ArrayList<>();
+        for (Pin pin : getPins()) {
+            if (pin == addPin) {
+                break;
+            }
+            if (pin.isSameClass(PinMap.class)) {
+                pins.add(pin);
+            }
+        }
+        return pins;
+    }
+
     @NonNull
     @Override
     public List<Pin> getDynamicTypePins() {
-        return Arrays.asList(mapPin, map2Pin, resultPin);
+        List<Pin> pins = new ArrayList<>(getMapPins());
+        pins.add(resultPin);
+        return pins;
     }
 
 }
